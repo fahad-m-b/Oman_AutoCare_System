a466e275185a296d0a5659de6d0d1c029800382a
Pls8 <storyingmasod@gmail.com>
2025-10-30 11:53:37 +0400

    add ServiceClass

---
+++ Oman_AutoCare_System/Models/ServiceClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oman_AutoCare_System.Models
+{
+    public class ServiceClass
+    {
+        [Key]
+        public int serviceID { get; set; }
+        public string Name { get; set; }
+        public int estimatedTime { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
