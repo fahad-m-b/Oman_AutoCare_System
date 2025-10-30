374197cf06dc72f38887149c91388fa6296afd19
fahad-m-b <141445845+fahad-m-b@users.noreply.github.com>
2025-10-30 11:56:59 +0400

    Invoice added

---
+++ Oman_AutoCare_System/Models/InvoiceClass.cs
@@ -0,0 +1,19 @@
+﻿using Oman_AutoCare_System.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oman_AutoCare_System.Models
+{
+    public class InvoiceClass
+    {
+        public int Invoice_ID { get; set; }
+        public decimal Total_Amount { get; set; }
+        public static decimal VAT = 0.05m;
+        public PaymentMethod paymentMethod { get; set; }
+        public DateOnly Invoice_Date { get; set; }
+        public WorkOrderClass WorkOrder_ID { get; set; }
+    }
+}
